4029e96a1a3db40db2f5ede572aea30d9200a047
jayson-ambrose <jayson.ambrose.87@gmail.com>
2023-04-26 18:02:10 -0600

    non-working changes

--- client/app/src/Search.js
+++ client/app/src/Search.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // import native components here
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 function Search({navigation}) {
 
+    const [text, setText] = useState('')
+
+    function handleSearch () {
+        console.log(text)
+    }
+
+    function handleChange(e) {
+        setText(e.target.value)
+    }
+
     return(
         <View style={styles.container}>
-            <Text>Hello World</Text>
+            <Text>Enter title to search: </Text>
+            <TextInput 
+                placeholder={'enter query'} 
+                onSubmitEditing={handleSearch}
+                onChange={handleChange}
+                value={text}
+            />
+            <Button 
+                title={'Search'} 
+                type={'submit'}
+                onPress={handleSearch}
+            />
         </View>
     )
-}
+}   
 
 export default Search
 
