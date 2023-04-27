9b21b12b380db271b8edfd63df95f2d7445187d3
jayson <jayson.ambrose.87@gmail.com>
2023-04-27 15:41:04 -0600

    working post to backlogs

--- server/app.py
+++ server/app.py
@@ -18,11 +18,6 @@ class Users(Resource):
 
         return make_response(user_list, 200)
     
-class Books(Resource):
-    #get list of all books (check if book is present before posting)
-    #post books to backend
-    pass
-
 class BooksById(Resource):
     #get book details from backend by isbn (used by backlog not search)
     pass
@@ -34,7 +29,41 @@ class Reviews(Resource):
 class ReviewsByBookId(Resource):
     #get reviews by book id
     pass
-    
+
+class Backlogs(Resource):
+
+    #backlog post should send in object {
+    # user_id
+    # isbn
+    # title
+    # author
+    # }
+
+    def post(self):
+        req_data = request.get_json()
+        book = Book.query.filter(Book.isbn == req_data['isbn']).one_or_none()
+        if not book:
+            new_book = Book(title = req_data['title'], author = req_data['author'], isbn = req_data['isbn'])
+            try:
+                db.session.add(new_book)
+                db.session.commit()
+
+                book = Book.query.filter(Book.isbn == req_data['isbn'].one_or_none())
+            except:
+                return make_response({"error": "something went wrong on our end"}, 500)
+            
+        backlog = Backlog(completed=0, user_id=req_data['user_id'], book_id=book.id)
+        try:
+            db.session.add(backlog)
+            db.session.commit()
+            return make_response(backlog.to_dict(only={'user.id', 'completed'}), 201)
+
+        except:
+            return make_response({"error": "something went wrong"}, 400)
+
+
 api.add_resource(Users, '/users')
+api.add_resource(Backlogs, '/backlogs')
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
