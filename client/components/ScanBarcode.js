aa9ddbc0d74d6d8005f56d9f9b54ba14f39d40e1
jayson <jayson.ambrose.87@gmail.com>
2023-05-08 20:55:02 -0600

    barcode scanner functional

--- client/components/ScanBarcode.js
+++ client/components/ScanBarcode.js
@@ -6,7 +6,6 @@ function ScanBarcode({navigation}) {
 
     const [hasPermission, setHasPermission] = useState(null)
     const [scanned, setScanned] = useState(false)
-    const [bookObject, setBookObject] = useState(null)
 
     useEffect(() => {
         const getBarCodeScannerPermissions = async () => {
@@ -17,33 +16,20 @@ function ScanBarcode({navigation}) {
         
     }, [])
 
-    const handleBarCodeScanned = (barcode) => {
-        const {type, data} = barcode;
-        () => setScanned(true);
-        console.log(`barcode type: ${type} \n barcode data: ${data}`)
-
-        if (scanned === true){
+    const handleBarCodeScanned = ({type, data}) => {
         
-            fetch(`https://openlibrary.org/search.json?isbn=${data}&limit=1`)
-            .then(resp => resp.json())
-            .then(bookInfo => {
-
-                const item = bookInfo.docs[0]
-
-                setBookObject({
-                        title: item?.title,
-                        isbn: item?.isbn[0],
-                        author: item?.author_name[0]
-                    }) 
-                    
-                console.log(bookObject)
-
-                navigation.navigate('Details', bookObject)
-
-                setTimeout(() => {setBookObject(null), 200})
-                })
-
-    }}
+        setScanned(true);
+        console.log(`barcode type: ${type} \n barcode data: ${data}`)
+        
+        fetch(`https://openlibrary.org/search.json?isbn=${data}&limit=1`)
+        .then(resp => resp.json())
+        .then(bookInfo => { 
+            
+            navigation.replace('Details', {title: bookInfo.docs[0].title, 
+            isbn: data, 
+            author: bookInfo.docs[0].author_name[0] })            
+        })
+    }
 
     if (hasPermission == null) {
         return <Text>Requesting for camera permissions...</Text>
@@ -55,7 +41,7 @@ function ScanBarcode({navigation}) {
     return(
         <View style={styles.container}>
             <BarCodeScanner
-                onBarCodeScanned = {scanned ? undefined: handleBarCodeScanned}
+                onBarCodeScanned = {scanned ? null: (barcode) => handleBarCodeScanned(barcode)}
                 style = {StyleSheet.absoluteFillObject}
             />
             {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
